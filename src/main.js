1d49517f0ebc5833ee150398f750ec0d1f74aa56
themurgsguy <ernest.da.cruz@gmail.com>
2021-05-04 10:24:46 +0100

    Installed and setup bootstrap-vue

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
 import App from './App.vue'
 
+import '@/assets/scss/app.scss'
+
 Vue.config.productionTip = false
+
+Vue.use(BootstrapVue)
 
 new Vue({
   render: h => h(App),
